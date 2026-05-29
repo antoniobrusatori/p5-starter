e15fa3cb51bd4189cd6eeabf75953840374167fd
antoniobrusatori <antoniobrusatori@students.berkeley.net>
2026-05-29 17:21:31 +0000

    final incomplete but tried

--- script.js
+++ script.js
@@ -1,65 +1,117 @@
-//preview: python -m http.server
 
-let myBall;
+// python -m  http.server
+let allBalls = [];
+let myFrameCount = 0;
+let obj, obj2;
+
 
 function setup() {
-  createCanvas(windowWidth-100, windowHeight-100);
+  createCanvas(windowWidth - 100, windowHeight - 100);
+  obj = new Obje(0, 50, 1700, 100);
+ obj2 = new Obje(0, 500, 1700, 100);
   background(30);
-  myBall = new Ball(width / 2, height / 2, 40);
+
+  for (let i = 0; i < 1; i++) {
+    allBalls.push(new Ball(random(width), random(height), 20));
+  }
 }
 
 function draw() {
+  myFrameCount++;
   background(30);
-  myBall.update();   // Calculate physics
-  myBall.checkKeys(); // Check for keyboard input
-  myBall.display();    // Draw the ball
+
+  
+  obj.move();
+  obj2.move();
+  obj.display();
+  obj2.display();
+
+  for (let i = 0; i < allBalls.length; i++) {
+    allBalls[i].checkKeys();
+    allBalls[i].update();
+    allBalls[i].checkEdges();
+    allBalls[i].display();
+  }
+}
+
+class Obje {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h; 
+    this.currentY = y;
+    this.offset = 0;
+  }
+ 
+  move() {
+  let smooth = sin(myFrameCount * 0.4) * 15;
+  let shake = random(-5, 5);
+  this.currentY = this.y + smooth + shake;
+}
+
+  display() {
+    fill(255, 0, 0);
+    rect(this.x, this.currentY, this.w, this.h);
+  }
 }
 
 class Ball {
   constructor(x, y, r) {
+    this.red = random(255);
+    this.blue = random(255);
+    this.green = random(255);
+
     this.pos = createVector(x, y);
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
+
     this.r = r;
-    this.topSpeed = 60;
-    this.friction = 0.99; 
+    this.topSpeed = 10;
+    this.friction = 1.00; 
   }
 
-  // Method to check keyboard input and apply forces
   checkKeys() {
-    let forceMagnitude = 12;
-    
-    if (keyIsDown(LEFT_ARROW))  this.applyForce(createVector(-forceMagnitude, 0));
+    let forceMagnitude = 0.5; // Reduced force for better control
+    if (keyIsDown(LEFT_ARROW)) this.applyForce(createVector(-forceMagnitude, 0));
     if (keyIsDown(RIGHT_ARROW)) this.applyForce(createVector(forceMagnitude, 0));
-    if (keyIsDown(UP_ARROW))    this.applyForce(createVector(0, -forceMagnitude));
-    if (keyIsDown(DOWN_ARROW))  this.applyForce(createVector(0, forceMagnitude));
+    if (keyIsDown(UP_ARROW)) this.applyForce(createVector(0, -forceMagnitude));
+    if (keyIsDown(DOWN_ARROW)) this.applyForce(createVector(0, forceMagnitude));
+    
+    if (myFrameCount === 1) {
+      this.applyForce(
+        createVector(
+          random(-forceMagnitude * 10, forceMagnitude * 10),
+          random(-forceMagnitude * 10, forceMagnitude * 10)
+        )
+      );
+    }
   }
 
-  // The "Force" pattern: Force adds to Acceleration
+  checkEdges() {
+    if (this.pos.x <= 0 || this.pos.x >= width) {
+      this.vel.x *= -1;
+    }
+    if (this.pos.y <= 0 || this.pos.y >= height) {
+      this.vel.y *= -1;
+    }
+  }
+
   applyForce(force) {
     this.acc.add(force);
   }
 
   update() {
-    // 1. Acceleration changes Velocity
     this.vel.add(this.acc);
-    
-    // 2. Limit the speed so it doesn't go infinite
     this.vel.limit(this.topSpeed);
-    
-    // 3. Velocity changes Position
     this.pos.add(this.vel);
-    
-    // 4. Apply friction (velocity decay)
+    this.acc.mult(0); 
     this.vel.mult(this.friction);
-    
-    // 5. Reset acceleration for the next frame
-    this.acc.mult(0);
   }
 
   display() {
-    fill(255, 150, 0);
+    fill(this.red, this.green, this.blue);
     noStroke();
-    ellipse(this.pos.x, this.pos.y, this.r);
+    ellipse(this.pos.x, this.pos.y, this.r * 2);
   }
-}
+} 
